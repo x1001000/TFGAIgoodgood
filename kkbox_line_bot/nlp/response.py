3092f55ead2d104b0f2b93f4d3c712f42ed14264
x1001000 <x1001000@gmail.com>
2018-09-18 18:56:12 +0800

    jokeResponse

--- kkbox_line_bot/nlp/response.py
+++ kkbox_line_bot/nlp/response.py
@@ -98,6 +98,21 @@ class WeatherResponse(object):
         return [TextSendMessage(text=self.response_text)]
 
 
+class jokeResponse(object):
+
+    def __init__(self, response_text, data_obj):
+        self.response_text = response_text
+        self.data_obj = data_obj
+
+    def __repr__(self):
+        return '<jokeResponse object: response_text = {}, data_obj = {}>'.format(
+                self.response_text,
+                reprlib.repr(self.data_obj))
+
+    def as_line_messages(self):
+        return [TextSendMessage(text=self.data_obj[0]['content'])]
+
+
 class openwebResponse(object):
 
     def __init__(self, response_text, data_obj):
