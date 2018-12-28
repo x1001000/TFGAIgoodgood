64af26ec164676fa12f2e35e58539acd838f8a03
x1001000 <x1001000@gmail.com>
2018-12-29 06:30:06 +0800

    ErrorResponse

--- kkbox_line_bot/nlp/response.py
+++ kkbox_line_bot/nlp/response.py
@@ -20,8 +20,7 @@ class ErrorResponse(object):
                 self.status_code)
 
     def as_line_messages(self):
-        return [TextSendMessage(text='ʕ•ᴥ•ʔ')]
-#        return [TextSendMessage(text=self.response_text)],
+        return [TextSendMessage(text=self.response_text)]#,
 #                TextSendMessage(text='DEBUG: status_code={}'.format(self.status_code))]
 
 
