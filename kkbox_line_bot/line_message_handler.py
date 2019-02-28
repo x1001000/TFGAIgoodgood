ac26e0c03e5ca16f331f1ee8c0c0575212d4e24c
x1001000 <x1001000@gmail.com>
2019-03-01 03:20:10 +0800

    hi

--- kkbox_line_bot/line_message_handler.py
+++ kkbox_line_bot/line_message_handler.py
@@ -1,6 +1,6 @@
+from random import choice
 import requests
 import logging
-
 from kkbox_line_bot import app
 from kkbox_line_bot.nlp import olami
 from kkbox_line_bot.nlp.error import NlpServiceError
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 line_bot_api = LineBotApi(app.config['LINE_CHANNEL_ACCESS_TOKEN'])
 webhook_handler = WebhookHandler(app.config['LINE_CHANNEL_SECRET'])
 
-
+hi = ['有！', '你也讚讚！你全家都讚讚！', '安安你好我是讚讚', '嗯哼！']
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     logger.debug('event: ' + str(event))
@@ -23,15 +23,18 @@ def handle_text_message(event):
     msg_txt = event.message.text.strip()
     try:
         if msg_txt == '讚讚' or msg_txt == 'TFGAI讚讚':
-            reply = TextSendMessage(text='有！' if msg_txt == '讚讚' else '你也讚讚！你全家都讚讚！')
+            reply = TextSendMessage(text=choice(hi))
         elif '北一最' in msg_txt or '北一誰最' in msg_txt:
             adj = msg_txt.split('最')[1]
             for x in '的是誰啊阿ㄚ嗎嘛ㄇ讚讚？?':
                 adj = adj.split(x)[0]
             if '=' in adj or '＝' in adj:
                 adj, who = adj.split('=' if '=' in adj else '＝')
-                requests.get(app.config['GOOGLE_SHEETS']+'?'+adj+'='+(who if who else 'instagr.am/1001000.io'))
-                reply = TextSendMessage(text='嗯哼！')
+                if adj and who:
+                    requests.get(app.config['GOOGLE_SHEETS']+'?'+adj+'='+who)
+                    reply = TextSendMessage(text='是喔！')
+                else:
+                    reply = TextSendMessage(text='蛤？')                    
             else:
                 who = requests.get(app.config['GOOGLE_SHEETS']+'?'+adj).text
                 reply = TextSendMessage(text=who)
