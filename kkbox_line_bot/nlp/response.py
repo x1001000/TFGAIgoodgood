94e6465b0b69b1534b2b4f7f3e75bcac53651efe
x1001000 <x1001000@gmail.com>
2019-03-13 07:13:01 +0800

    selectionResponse

--- kkbox_line_bot/nlp/response.py
+++ kkbox_line_bot/nlp/response.py
@@ -155,9 +155,9 @@ class selectionResponse(object):
                 reprlib.repr(self.data_obj))
 
     def as_line_messages(self):
-        response_msg = TextSendMessage(text=len(self.data_obj))#self.response_text)
+        response_msg = TextSendMessage(text=self.response_text)
         template_msg = self._create_template_message() if self.data_obj else None
-        return [response_msg, template_msg] #if template_msg else [response_msg]
+        return [template_msg]#[response_msg, template_msg]
 
     def _create_template_message(self):
         return TemplateSendMessage(alt_text='讚讚的傳送門們',
