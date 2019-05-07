9bbb73f9087a5def54d034a7b87af967e5498dfc
x1001000 <x1001000@gmail.com>
2019-05-07 23:31:19 +0800

    whiteeye.gif

--- kkbox_line_bot/line_message_handler.py
+++ kkbox_line_bot/line_message_handler.py
@@ -27,6 +27,10 @@ def handle_text_message(event):
             reply = ImageSendMessage(
                 original_content_url='https://www.1001000.io/img/cucumber.gif',
                 preview_image_url='https://www.1001000.io/img/cucumber.jpg')
+        elif '發財' in msg_txt or '發大財' in msg_txt:
+            reply = ImageSendMessage(
+                original_content_url='https://www.1001000.io/img/whiteeye.gif',
+                preview_image_url='https://www.1001000.io/img/whiteeye.gif')
         elif msg_txt == '讚讚' or msg_txt == 'TFGAI讚讚':
             reply = TextSendMessage(text=choice(hi))
         elif '北一最' in msg_txt or '北一誰最' in msg_txt:
