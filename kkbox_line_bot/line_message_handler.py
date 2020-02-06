db2852eb29e9adc9b0048dc369aad0ba7342d7f1
x1001000 <x1001000@gmail.com>
2020-02-07 02:57:07 +0800

    ig

--- kkbox_line_bot/line_message_handler.py
+++ kkbox_line_bot/line_message_handler.py
@@ -1,6 +1,5 @@
-from random import choice
-import requests
-import logging
+import requests, random, logging
+
 from kkbox_line_bot import app
 from kkbox_line_bot.nlp import olami
 from kkbox_line_bot.nlp.error import NlpServiceError
@@ -13,7 +12,13 @@ logger = logging.getLogger(__name__)
 line_bot_api = LineBotApi(app.config['LINE_CHANNEL_ACCESS_TOKEN'])
 webhook_handler = WebhookHandler(app.config['LINE_CHANNEL_SECRET'])
 
-hi = ['有！', '你也讚讚！你全家都讚讚！', '安安你好我是讚讚', '嗯哼！']
+def ig():
+    r = requests.get('https://www.instagram.com/explore/locations/262402199/')
+    for line in r.text.splitlines():
+        if '>window._sharedData' in line:
+            shortcode = random.choice(line.split('shortcode":"')[1:])[:11]
+            return f'https://www.instagram.com/p/{shortcode}'
+
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     logger.debug(event)
@@ -31,9 +36,7 @@ def handle_text_message(event):
         #    reply = ImageSendMessage(
         #        original_content_url='https://www.1001000.io/img/whiteeye.gif',
         #        preview_image_url='https://www.1001000.io/img/whiteeye.gif')
-        if msg_txt == '讚讚' or msg_txt == 'TFGAI讚讚':
-            reply = TextSendMessage(text=choice(hi))
-        elif '北一最' in msg_txt or '北一誰最' in msg_txt:
+        if '北一最' in msg_txt or '北一誰最' in msg_txt:
             adj = msg_txt.split('最')[1]
             for x in '的是誰呢ㄋ啊阿ㄚ嗎嘛ㄇ？?':
                 adj = adj.split(x)[0]
@@ -47,9 +50,10 @@ def handle_text_message(event):
             else:
                 who = requests.get(app.config['GOOGLE_SHEETS']+'?'+adj).text
                 reply = TextSendMessage(text=who)
-        elif msg_txt[:2] in ['讚讚', '阿信', '石頭', '怪獸', '冠佑', '瑪莎', '膺任', '百千']:
-            resp = olami_svc(msg_txt[2:])
-            reply = resp.as_line_messages()
+        elif '讚讚' in msg_txt:
+            #resp = olami_svc(msg_txt[2:])
+            #reply = resp.as_line_messages()
+            reply = TextSendMessage(text=ig())
     except NlpServiceError as e:
         err_msg = 'NLP service is currently unavailable: {}'.format(repr(e))
         logger.error(err_msg)
